ba8223aa4a8beb2f7b7057d4cedbffd60e44c5f8
KamilKruszewski <61495825+KamilKruszewski@users.noreply.github.com>
2020-06-10 22:35:00 +0200

    Add files via upload

---
+++ js/controllers.js
@@ -0,0 +1,49 @@
+var portfolioApp = angular.module('portfolioApp',[]);
+
+portfolioApp.controller('GalleryListCtrl',function($scope) {
+$scope.galleries = [
+		{ "title":"Parasite",
+			"when":"2020",
+			"thumbnailUrl":"pict/ang/kol1.jpg"
+		},
+		{  "title":"Kształ wody",
+			"when":"2018",
+			"thumbnailUrl":"pict/ang/kol3.jpg"
+		},
+		{ "title":"Spotlight",
+			"when":"2016",
+			"thumbnailUrl":"pict/ang/kol5.jpg"
+		},
+		{  "title":"Greenboon",
+			"when":"2019",
+			"thumbnailUrl":"pict/ang/kol2.jpg"
+		},
+		{ "title":"Moonlight",
+			"when":"2017",
+			"thumbnailUrl":"pict/ang/kol4.jpg"
+		},
+		{  "title":"Birdman",
+			"when":"2015",
+			"thumbnailUrl":"pict/ang/kol6.jpg"
+		},
+		];
+		$scope.length=Object.keys($scope.galleries).length;
+		$scope.sortList = [
+		{
+			'label':'Alfabetycznie',
+			'value':'title'
+		},
+		{
+			'label':'Najstarsze',
+			'value':'when'
+		},
+		{
+			'label':'Najnowsze',
+			'value':'-when'
+			}
+		];
+
+		$scope.orderProp = 'when';
+});
+
+

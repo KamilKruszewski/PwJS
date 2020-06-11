af5ceff73b9e7923ce0019cb1a05586ba8c74066
KamilKruszewski <61495825+KamilKruszewski@users.noreply.github.com>
2020-06-11 15:10:46 +0200

    Update controllers.js

--- js/controllers.js
+++ js/controllers.js
@@ -4,17 +4,14 @@ portfolioApp.controller('GalleryListCtrl',function($scope) {
 $scope.galleries = [
 		{ "title":"Parasite",
 			"when":"2020",
-		 	"ocena":"8,6",
 			"thumbnailUrl":"pict/ang/kol1.jpg"
 		},
 		{  "title":"Kształ wody",
 			"when":"2018",
-		 	"ocena":"7,3",
 			"thumbnailUrl":"pict/ang/kol3.jpg"
 		},
 		{ "title":"Spotlight",
 			"when":"2016",
-		 	"ocena":"8,1",
 			"thumbnailUrl":"pict/ang/kol5.jpg"
 		},
 		{  "title":"Greenboon",
@@ -24,12 +21,10 @@ $scope.galleries = [
 		},
 		{ "title":"Moonlight",
 			"when":"2017",
-		 	"ocena":"7,4",
 			"thumbnailUrl":"pict/ang/kol4.jpg"
 		},
 		{  "title":"Birdman",
 			"when":"2015",
-		 	"ocena":"7,7",
 			"thumbnailUrl":"pict/ang/kol6.jpg"
 		},
 		];
